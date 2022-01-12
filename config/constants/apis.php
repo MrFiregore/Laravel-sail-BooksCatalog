8dc8eeec29c0cded8ee09fedce52ed02537b62ea
Firegore <firegore2@gmail.com>
2022-01-12 17:31:52 +0100

    Removed unused API constants

--- config/constants/apis.php
+++ config/constants/apis.php
@@ -14,9 +14,6 @@
         'ERRCODE_INFOS_PARAM_INVALID'             => '-13',
         'ERRCODE_INFOS_ROUTE_MODEL_NOT_FOUND'     => '-14',
 
-        'ERRCODE_TELEGRAM_NOT_LOGGED'        => '-20',
-        'ERRCODE_TELEGRAM_CHANNEL_NOT_FOUND' => '-21',
-
 
         'REST_ERRORS' => [
 
@@ -32,7 +29,6 @@
             '-13' => ['msg' => 'The value is invalid. check it value and format'],
             '-14' => ['msg' => 'The given id not match with any element'],
 
-            '-20' => ['msg' => 'Don\'t have a Telegram session started'],
             '-21' => ['msg' => 'The given channel not found'],
         ],
 
